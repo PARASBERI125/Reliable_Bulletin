77279dbe60a20d79c93998f691ca0f1bdf1f1e74
paras beri <130133581+PARASBERI125@users.noreply.github.com>
2024-10-29 18:19:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API_KEY = "3066149e84624a398f091db2d94fd06a";
 const url = "https://newsapi.org/v2/everything?q=";
 window.addEventListener("load", () => fetchNews("India"));
 async function fetchNews(query) {
-  const res = await fetch(`${url} ${query}&apiKey=${API_KEY}`);
+  const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
   const data = await res.json();
   console.log(data);
   bindData(data.articles);
